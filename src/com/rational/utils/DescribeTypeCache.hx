1f319e9fd85d996acad4cc7c6737b5d68fda9260
Andy Sonnenburg <andy22286@gmail.com>
2010-02-19 08:29:16 -0600

    Fix for TypedDictionary

--- src/com/rational/utils/DescribeTypeCache.hx
+++ src/com/rational/utils/DescribeTypeCache.hx
@@ -1,7 +1,7 @@
 package com.rational.utils;
 
 import flash.Error;
-import flash.utils.TypedDictionary;
+import flash.utils.Dictionary;
 import flash.xml.XML;
 import flash.xml.XMLList;
 
@@ -10,22 +10,49 @@ using Type;
 class DescribeTypeCache<T> {
 	
 	private var factory:Class<Dynamic> -> XML -> T;
-	private var recordCache:TypedDictionary<String, T>;
+	private var recordCache:TypedDictionary<Class<Dynamic>, T>;
 
 	public function new(factory:Class<Dynamic> -> XML -> T) {
 		this.factory = factory;
-		recordCache = new TypedDictionary<String, T>();
+		recordCache = new TypedDictionary<Class<Dynamic>, T>();
 	}
 
 	public function describeType(type:Class<Dynamic>):T {
-		var className:String = DescribeTypeTools.getQualifiedClassName(type);
 		var record:T;
-		if ((record = recordCache.get(className)) != null) {
+		if ((record = recordCache.get(type)) != null) {
 			return record;
 		}
 		var typeDescription:XML = DescribeTypeTools.describeType(type);
 		record = factory(type, typeDescription);
-		recordCache.set(className, record);
+		recordCache.set(type, record);
 		return record;
 	}		
+}
+
+class TypedDictionary<K,T> extends Dictionary, implements Dynamic<T> {
+
+	public inline function get( k : K ) : Null<T> {
+		return this[cast k];
+	}
+
+	public inline function set( k : K, v : T ) {
+		this[cast k] = v;
+	}
+
+	public inline function exists( k : K ) {
+		return this[cast k] != null;
+	}
+
+	public inline function delete( k : K ) {
+		untyped __delete__(this,k);
+	}
+
+	public inline function keys() : Array<K> {
+		return untyped __keys__(this);
+	}
+
+	public function iterator() : Iterator<K> {
+		return keys().iterator();
+	}
+
 }
